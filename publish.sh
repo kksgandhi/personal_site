f8cfaa656a3f2bb153ce8bf86a92c9e3f747982f
Kutub Gandhi <kksgandhi@gmail.com>
2021-03-12 12:42:29 -0500

    automatic commit

--- publish.sh
+++ publish.sh
@@ -1,11 +1,11 @@
-#!/bin/zsh
+#!/bin/bash
 git commit -am "automatic commit"
 
 rm -r /tmp/temp-obsidian
 git clone ./.git /tmp/temp-obsidian
 # sed -i 's/\[\[\([^|]*\)|\([^]]*\)\]\]/\2 ([[\1]])/' /tmp/temp-obsidian/*.md
 # perl -pie 's/\[\[([^|\]]+)\|([^\]]+\s+[^\]]+)\]\]/\2 ([[\1]])/' /tmp/temp-obsidian/*.md
-sd '\[\[([^|\]]+)\|([^\]]+\s+[^\]]+)\]\]' '$2 ([[$1]])' /tmp/temp-obsidian/*.md
+/home/kksgandhi/.cargo/bin/sd '\[\[([^|\]]+)\|([^\]]+\s+[^\]]+)\]\]' '$2 ([[$1]])' /tmp/temp-obsidian/*.md
 rm -r /tmp/temp-obsidian/.neuron
 neuron gen -d /tmp/temp-obsidian/
 
