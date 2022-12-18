ac0b94797cf7ef2acd5cc55abfc9d4720cd529be
kksgandhi <kksgandhi@users.noreply.github.com>
2022-12-18 23:49:15 +0000

    deploy: 492baef9a73c8531cd844391cb767c354311914f

---
+++ neuron.dhall
@@ -0,0 +1,7 @@
+{ siteTitle = "Kutub Gandhi's personal website"
+, author = Some "Kutub Gandhi"
+, siteBaseUrl = Some "https://kksgandhi.github.io/personal_site"
+-- List of theme colors: https://neuron.zettel.page/2014601.html
+, theme = "brown"
+-- This is used in the "edit" button
+}
---
+++ publish.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+git add *.md
+git add -u
+git commit -am "automatic commit"
+
+rm -rf /tmp/temp-obsidian
+git clone /home/kksgandhi/Obsidian /tmp/temp-obsidian
+# sed -i 's/\[\[\([^|]*\)|\([^]]*\)\]\]/\2 ([[\1]])/' /tmp/temp-obsidian/*.md
+# perl -pie 's/\[\[([^|\]]+)\|([^\]]+\s+[^\]]+)\]\]/\2 ([[\1]])/' /tmp/temp-obsidian/*.md
+# /home/kksgandhi/.cargo/bin/sd '\[\[([^|\]]+)\|([^\]]+)\]\]' '$2 ([[$1]])' /tmp/temp-obsidian/*.md
+
+python3 post_process.py /home/kksgandhi/Obsidian /tmp/temp-obsidian
+
+rm -rf /tmp/temp-obsidian/.neuron
+neuron gen -d /tmp/temp-obsidian/
+
+rm -rf /home/kksgandhi/Obsidian/.neuron
+cp -r /tmp/temp-obsidian/.neuron /home/kksgandhi/Obsidian 
+git add /home/kksgandhi/Obsidian/.neuron/*
+git commit -am "automatic commit"
+
+git push origin
+
+git status
