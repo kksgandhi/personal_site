3098c0ed3d52a78bbc530c88f31db687c29173a0
Kutub Gandhi <kksgandhi@gmail.com>
2023-03-27 09:38:30 -0400

    automatic commit

--- publish.sh
+++ publish.sh
@@ -5,6 +5,7 @@ git commit -am "automatic commit"
 
 rm -rf /tmp/temp-obsidian
 git clone /home/kksgandhi/Obsidian /tmp/temp-obsidian
+git -C /tmp/temp-obsidian pull
 # sed -i 's/\[\[\([^|]*\)|\([^]]*\)\]\]/\2 ([[\1]])/' /tmp/temp-obsidian/*.md
 # perl -pie 's/\[\[([^|\]]+)\|([^\]]+\s+[^\]]+)\]\]/\2 ([[\1]])/' /tmp/temp-obsidian/*.md
 # /home/kksgandhi/.cargo/bin/sd '\[\[([^|\]]+)\|([^\]]+)\]\]' '$2 ([[$1]])' /tmp/temp-obsidian/*.md
